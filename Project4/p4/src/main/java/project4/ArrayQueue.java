f327b4796f9f10110deef5f95553f1f7ca24c62b
Anderson Levasseur <anderson.levasseur@gmail.com>
2023-10-17 20:10:07 -0400

    removed rearIndex variable, added size variable

--- Project4/p4/src/main/java/project4/ArrayQueue.java
+++ Project4/p4/src/main/java/project4/ArrayQueue.java
@@ -26,7 +26,7 @@ public class ArrayQueue<E> implements Queue<E> {
     private Object[] arrayQueue = new Object[5];
     // frontIndex actually pointing at front object
     // rearIndex actually pointing at rear object
-    private int frontIndex, rearIndex, arraySize;
+    private int frontIndex, size, arrayLength;
 
     /**
      * @param None - ArrayQueue Constructor, maybe not needed?
@@ -34,8 +34,8 @@ public class ArrayQueue<E> implements Queue<E> {
      */
     public ArrayQueue() {
         frontIndex = 0;
-        rearIndex = 0;
-        arraySize = 5;
+        size = 0;
+        arrayLength = 5;
     }
 
     /**
@@ -49,15 +49,15 @@ public class ArrayQueue<E> implements Queue<E> {
      */
     public void enqueue(E element) throws InvalidDataException {
         // to account for 0 index
-        if (size() == arraySize - 1) {
+        if (size == arrayLength) {
             resize();
         }
         if (element == null) {
             throw new InvalidDataException();
         }
 
-        arrayQueue[rearIndex % arraySize] = element;
-        rearIndex++;
+        arrayQueue[(frontIndex + size) % arrayLength] = element;
+        size++;
     }
 
     /**
@@ -72,8 +72,9 @@ public class ArrayQueue<E> implements Queue<E> {
         if (isEmpty()) {
             throw new QueueEmptyException();
         }
-        Object element = arrayQueue[frontIndex % arraySize];
-        frontIndex++;
+        Object element = arrayQueue[frontIndex];
+        frontIndex = (frontIndex + 1) % arrayLength;
+        size--;
         return (E) element;
     }
 
@@ -97,7 +98,7 @@ public class ArrayQueue<E> implements Queue<E> {
      *         ArrayQueue
      */
     public int size() {
-        return (rearIndex - frontIndex) % arraySize;
+        return size;
     }
 
     /**
@@ -106,20 +107,18 @@ public class ArrayQueue<E> implements Queue<E> {
      * @return True or False
      */
     public boolean isEmpty() {
-        return rearIndex == frontIndex;
+        return size == 0;
     }
 
     private void resize() {
-        Object[] temp = new Object[arraySize * 2];
+        Object[] temp = new Object[arrayLength * 2];
 
-        for (int i = frontIndex; i < arraySize + frontIndex; i++) {
-            temp[i - frontIndex] = arrayQueue[i % arraySize];
+        for (int i = frontIndex; i < arrayLength + frontIndex; i++) {
+            temp[i - frontIndex] = arrayQueue[i % arrayLength];
         }
 
         frontIndex = 0;
-        // could also call size() to amke things more readable, this is faster though
-        rearIndex = arraySize - 1;
-        arraySize *= 2;
+        arrayLength *= 2;
 
         arrayQueue = temp;
     }
